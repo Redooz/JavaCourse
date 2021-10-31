a72399423afae497d97effce1acbcb1d6258a900
Nicolás Olmos <87039517+Redooz@users.noreply.github.com>
2021-10-30 19:01:49 -0500

    Delete helloWorld directory

--- helloWorld/src/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-    public static void main(String args[]){
-        System.out.println("Hola Mundo desde Java");
-        int number = 0;
-        System.out.println(number);
-        number = number+1; 
-        System.out.println(number);
-    }
-}
