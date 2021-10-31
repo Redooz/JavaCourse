18e70619d8cf046f54974634e356a9169ba514f9
Nicolás Olmos <yohannicolas2003@gmail.com>
2021-10-30 19:03:36 -0500

    Fixed .gitignore and changed number from 0 to 10

--- helloWorld/src/HelloWorld.java
+++ helloWorld/src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String args[]){
         System.out.println("Hola Mundo desde Java");
-        int number = 0;
+        int number = 10;
         System.out.println(number);
         number = number+1; 
         System.out.println(number);
