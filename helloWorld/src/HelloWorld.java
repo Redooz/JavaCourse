21107eba52075eb33cb5e5870cb4ed9e87636152
Nicolás Olmos <yohannicolas2003@gmail.com>
2021-10-30 19:29:00 -0500

    Ended my 8th class, today i learned how to use int and String

--- helloWorld/src/HelloWorld.java
+++ helloWorld/src/HelloWorld.java
@@ -1,11 +1,12 @@
 public class HelloWorld {
     public static void main(String args[]){
-        System.out.println("Hola Mundo desde Java");
         int number = 10;
         String myName = "Nicolas";
+        System.out.println("Hola Mundo desde Java");
         System.out.println(number);
         number = number+1; 
         System.out.println(number);
         System.out.println("Hi "+myName);
+        System.out.println("Bye "+myName);
     }
 }
