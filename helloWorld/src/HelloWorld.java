733e6478e84f2864597275b1f41551cd773bdf7f
Nicolás Olmos <yohannicolas2003@gmail.com>
2021-10-30 19:23:06 -0500

    Added an string to my code, I'm learning how to use it

--- helloWorld/src/HelloWorld.java
+++ helloWorld/src/HelloWorld.java
@@ -2,8 +2,10 @@ public class HelloWorld {
     public static void main(String args[]){
         System.out.println("Hola Mundo desde Java");
         int number = 10;
+        String myName = "Nicolas";
         System.out.println(number);
         number = number+1; 
         System.out.println(number);
+        System.out.println("Hi "+myName);
     }
 }
